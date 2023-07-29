07b03df9d9f37a223ea89430b8215b91726ba796
lije <lije0009@gmail.com>
2023-07-29 11:04:49 +0900

    Initial commit

---
+++ src/main/java/com/study/base/boot/SpringbootBaseStudyApplication.java
@@ -0,0 +1,13 @@
+package com.study.base.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBaseStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBaseStudyApplication.class, args);
+    }
+
+}
