97c45a2c266df0954d1931db4a812eaf8cc5cff7
lije <lije0009@gmail.com>
2023-08-03 07:32:07 +0900

    문제(2)

--- src/main/java/com/study/base/boot/aggregations/v1/order/presentation/OrderController.java
+++ src/main/java/com/study/base/boot/aggregations/v1/order/presentation/OrderController.java
@@ -1,6 +1,7 @@
 package com.study.base.boot.aggregations.v1.order.presentation;
 
 import com.study.base.boot.config.annotations.Get;
+import com.study.base.boot.config.annotations.Post;
 import com.study.base.boot.config.annotations.RestApi;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,5 +16,10 @@ public class OrderController {
     @Get
     public List<String> getOrders(){
         return List.of("A","B","C");
+    }
+    @Post
+    public List<String> createOrders(List<String> reqOrders){
+
+        return reqOrders;
     }
 }
